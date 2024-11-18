1443454c4e486b8efbdf96b0bb13ad9c173af62b
RAM <pradipsp67@gmail.com>
2024-11-18 17:42:49 +0530

    added program

--- src/test/java/javaQuetionsPractice/FindNumberOfCharacters.java
+++ src/test/java/javaQuetionsPractice/FindNumberOfCharacters.java
@@ -1,9 +1,25 @@
 package javaQuetionsPractice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class FindNumberOfCharacters {
 
     public static void main (String [] args)
     {
-        System.out.println("Test ...");
+        String name="welcome india";
+        char[] chr=name.toCharArray();
+        Map<Character,Integer> countOfChars=new HashMap<>();
+        for (char c:chr)
+        {
+            if (c!=' ')
+            {
+               countOfChars.put(c, countOfChars.getOrDefault(c,0)+1) ;
+            }
+        }
+        System.out.println(countOfChars);
+
+
+
     }
 }
